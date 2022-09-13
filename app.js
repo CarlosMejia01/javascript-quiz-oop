0cd2d863132a04f21af806c0a58eb04a6b7b8fd2
Carlos Mejia <cmejia@equitel.com.co>
2022-09-12 20:00:33 -0500

    define and import the data structure for questions and answers

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-console.log('Hello World');
+import { data } from "./data/data.js ";
+
+console.log(data);
---
+++ data/data.js
@@ -0,0 +1,17 @@
+export const data = [
+	{
+		question: "Cual de las siguientes opciones no es un editor de codigo?",
+		choices: ["vim", "vscode", "emacs", "word"],
+		answer: "word",
+	},
+	{
+		question: "Que lenguaje no es orientado a objetos?",
+		choices: ["java", "haskell", "c++", "python"],
+		answer: "haskell",
+	},
+	{
+		question: "Que lenguajes funcionan para estilizar sitios web?",
+		choices: ["stylus", "less", "sass", "postCSS", "nextCSS"],
+		answer: "postCSS",
+	},
+];
